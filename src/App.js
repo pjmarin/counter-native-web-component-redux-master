f216dbc7874be08a968e4b20dee39939293ffbb3
pjmarin <39600116+pjmarin@users.noreply.github.com>
2022-09-23 08:26:03 +0200

    Added project files

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import '@webcomponents/webcomponentsjs';
+import Counter from './components/Counter';
+import store from './store';
+
+class App {
+
+  constructor() {
+    this.initComponents();
+    store.subscribe(()=>{      
+      const altogether = store.getState().reduce((acc, currentCounter)=>{ return currentCounter.counter + acc}, 0)
+      this.changeAltogether(altogether);
+    });
+  }
+
+  changeAltogether(altogether) {
+    document.getElementById('altogether').innerHTML = altogether;
+    console.log(store.getState());
+  }
+
+  initComponents() {
+    window.customElements.define('my-counter', Counter);
+  }
+
+}
+
+(function(){
+  new App();
+})();
+
+
